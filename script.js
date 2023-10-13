0d8b08341c297fe58de217b6cd03297c8484af8a
Aline Lenzi <alinelenzi@gmail.com>
2023-10-13 15:04:43 -0300

    requisação de preenchimento do formulário e salvar as informações no localStorage

--- script.js
+++ script.js
@@ -5,3 +5,26 @@ inputCheck.addEventListener('click', () =>{
     const modo = inputCheck.checked ? 'dark' : 'light';
     elemento.setAttribute("data-bs-theme", modo)
 })
+
+const formulario = document.querySelector("[data-formulario]"); 
+
+formulario.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    var nome = event.target.elements["nome"].value;
+    var email = event.target.elements["email"].value;
+    var telefone = event.target.elements["telefone"].value;
+
+    let cadastro = new Array();
+
+    if(localStorage.hasOwnProperty("cadastro")){
+        cadastro = JSON.parse(localStorage.getItem("cadastro"))
+    }
+    cadastro.push({nome, email, telefone})
+
+    localStorage.setItem("cadastro", JSON.stringify(cadastro))
+    alert("Cadastro realizado com sucesso!")
+    
+    window.location.href = './index.html';
+
+});
